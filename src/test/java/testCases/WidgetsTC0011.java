9441fa83d986f9329f64929103679f70421f2874
ankit <ankitpawar193@gmail.com>
2025-09-19 12:33:36 +0530

    local commit 1a

--- src/test/java/testCases/WidgetsTC0011.java
+++ src/test/java/testCases/WidgetsTC0011.java
@@ -5,9 +5,9 @@ import org.testng.annotations.Test;
 import pageObjects.Widgets;
 import testBase.BaseClass;
 
-public class WidgetsTC0011 extends BaseClass{
-	
-	@Test
+public class WidgetsTC0011 extends BaseClass {
+
+	@Test(priority = 12)
 	public void widgetsMethod() {
 		Widgets w = new Widgets(driver);
 		w.clickWidgetsLink();
